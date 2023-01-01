627ccaa58931da1f7fde5352606065500566cd91
gofort <kurnosov_bogdan@hotmail.com>
2023-01-01 20:16:27 +0100

    add websocket

---
+++ ws/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+func main() {
+	http.HandleFunc("/ws", wsHandler)
+
+	panic(http.ListenAndServe(":8080", nil))
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := websocket.Upgrade(w, r, w.Header(), 1024, 1024)
+	if err != nil {
+		http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
+	}
+
+	i := 0
+
+	for {
+
+		if err = conn.WriteJSON(&map[string]string{
+			"message": "hello world " + fmt.Sprint(i),
+		}); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		time.Sleep(time.Second)
+		i++
+	}
+}
